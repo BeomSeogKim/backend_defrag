940c65dd2c3d63348ee61df71936569e47716420
BeomSeogKim <kbs4520@daum.net>
2023-04-24 16:49:50 +0900

    Enum Validator 구현

--- enum-validator/src/main/java/com/tommykim/enumvalidator/web/RequestDto.java
+++ enum-validator/src/main/java/com/tommykim/enumvalidator/web/RequestDto.java
@@ -1,5 +1,7 @@
 package com.tommykim.enumvalidator.web;
 
+import com.tommykim.enumvalidator.domain.Gender;
+import com.tommykim.enumvalidator.validator.EnumValue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -9,6 +11,6 @@ public class RequestDto {
     @NotNull(message = "null 일 수 없습니다.")
     @NotBlank(message = "공백 입력은 불가합니다.")
     private String email;
-
+    @EnumValue(enumClass = Gender.class, message = "유효하지 않은 성별입니다.", ignoreCase = true)
     private String gender;
 }
