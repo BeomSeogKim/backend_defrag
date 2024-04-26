ffed161b0e06130b09f9e3ab89413aa595dd0a95
BeomSeogKim <kbs4520@daum.net>
2024-04-26 14:24:47 +0900

    init

---
+++ jpa-auditor-with-security/src/main/java/com/example/jpaauditorwithsecurity/JpaAuditorWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaauditorwithsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAuditorWithSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaAuditorWithSecurityApplication.class, args);
+    }
+
+}
---
+++ jpa-auditor-with-security/src/test/java/com/example/jpaauditorwithsecurity/JpaAuditorWithSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaauditorwithsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAuditorWithSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
